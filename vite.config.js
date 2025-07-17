854c8ee0e75deac88c541965a7b50b8f1bb33649
Paola Perez Valencia <paola.pv1999@gmail.com>
2025-07-17 12:26:30 -0600

    UPDATE Changing base to serve app

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "./", // 👈🏽 ESTA LÍNEA ES FUNDAMENTAL PARA PRODUCCIÓN EN HEROKU
 });
