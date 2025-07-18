22102b650ca83e15a88ce9be3bc784f5070faec7
Paola Perez Valencia <paola.pv1999@gmail.com>
2025-07-18 15:17:31 -0600

    FIX FAQ Section

--- src/data/faqs.js
+++ src/data/faqs.js
@@ -31,13 +31,13 @@ export const faqs = [
   },
   {
     id: 6,
-    question: "Where can I get bloodwork done?",
+    question: "Do I have to stop drinking alcohol?",
     answer:
       "Yes. We require people to stop drinking alcohol for a minimum of 5 days prior to arrival.If you are at risk of experiencing alcohol withdrawals, such as tremors and seizures, this must be managed with medication and the assistance of a physician. We can provide supervised alcohol detoxification on-site at our private facilities.",
   },
   {
     id: 7,
-    question: "Where can I get bloodwork done?",
+    question: "How can I best prepare?",
     answer:
       "We provide everyone who comes to our facilities with access to an ibogaine-informed coach who we recommend that you meet with at least 2 times prior to arrival, and continue to meet with after you leave our facility. Your coach will help you get to know you and your reasons for taking ibogaine, and help you with practical and psychological preparations before you arrive.Other recommendations for preparation may come from our medical team, and include recommendations regarding medications, diet, etc.In general, we find that our treatments can help to create strong points of change in people’s lives. Working to solidify those changes as much as possible prior to coming can make these transitions much easier. This may include having a new place to stay when you return home, or it may be simpler changes such as implementing a gym routine or diet changes. Whatever it is, your coach will help you to clarify these goals so you can make the most out of your experience.",
   },
