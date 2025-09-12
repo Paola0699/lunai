8c06b43a824b298c1c5a645f0c1595e9f963c451
Paola Perez Valencia <paola.pv1999@gmail.com>
2025-09-11 19:25:43 -0600

    UPDATE Removing navbar

--- src/pages/otherPages/main-pages-404/page.jsx
+++ src/pages/otherPages/main-pages-404/page.jsx
@@ -14,53 +14,6 @@ export default function MainPageNotFound() {
       <div className="theme-main">
         <div className="page" id="top">
           <>
-            <nav className="main-nav dark light-after-scroll transparent stick-fixed wow-menubar wch-unset">
-              <div className="main-nav-sub full-wrapper">
-                {/* Logo  (* Add your text or image to the link tag. Use SVG or PNG image format. 
-              If you use a PNG logo image, the image resolution must be equal 200% of the visible logo
-              image size for support of retina screens. See details in the template documentation. *) */}
-                <div className="nav-logo-wrap local-scroll">
-                  <Link to={`/`} className="logo">
-                    <img
-                      src="/assets/images/logo-white.svg"
-                      alt="Your Company Logo"
-                      width={105}
-                      height={34}
-                      className="logo-white"
-                    />
-                    <img
-                      src="/assets/images/logo-dark.svg"
-                      alt="Your Company Logo"
-                      width={105}
-                      height={34}
-                      className="logo-dark"
-                    />
-                  </Link>
-                </div>
-                {/* Mobile Menu Button */}
-                <div className="mobile-nav" role="button" tabIndex={0}>
-                  <i className="mobile-nav-icon" />
-                  <span className="visually-hidden">Menu</span>
-                </div>
-                {/* Main Menu */}
-                <div className="inner-nav desktop-nav">
-                  <ul className="clearlist scroll-nav local-scroll justify-content-end">
-                    <li className="active">
-                      <a href="mailto:elitethemes24@gmail.com">
-                        <i className="mi-email align-center" />
-                        elitethemes24@gmail.com
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="mi-call align-center" /> 0307-567-890
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                {/* End Main Menu */}
-              </div>
-            </nav>
             {/* End Navigation Panel */}
             <main id="main">
               {/* Home Section */}
